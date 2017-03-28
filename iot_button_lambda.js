e742c7042f969844212e561b08b0e0b058290ff1
deskaran <deskaran@amazon.com>
2017-03-27 19:20:23 -0700

    Updated to support local time zones
    Now supports manually entering time zone difference from UTC in hours, and code displays local time accordingly in text messages.

--- iot_button_lambda.js
+++ iot_button_lambda.js
@@ -1,6 +1,6 @@
 /*
 Lambda Function to use AWS IoT button to send check-in/check-out notifications for a hotel/AirBnB
-2017-03-15 - github.com/deskaran
+2017-03-30 - github.com/deskaran
 This code is being made available to use at your own risk. You are responsible for any costs incurred or damages caused by using this code (It is fairly safe and harmless, but just in case.)
 */
 
@@ -9,7 +9,7 @@ This code is being made available to use at your own risk. You are responsible f
 const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({ apiVersion: '2010-03-31' });
 
-// CHANGE THIS! Enter your phone number. Include country and area code
+// !!CHANGE THIS!! Enter your phone number. Include country and area code.
 const PHONE_NUMBER = '1-999-999-9999'; 
 
 exports.handler = (event, context, callback) => {
@@ -23,27 +23,44 @@ exports.handler = (event, context, callback) => {
 
     console.log(`Sending SMS to ${PHONE_NUMBER}`);
 
-    // Gets current time in UTC
     var currentTime = new Date();
-    // NOTE: Currently Lambda supports only UTC. Local time support will be added when available
+    // NOTE: all Lambda functions run on UTC
 
-    // OPTIONAL! Manually enter name of preferred timezone if different from UTC
-    var localTimeZone = 'PST';
-
-    // Extracts hour, minutes, seconds to transform to local format
     var currentHour = currentTime.getHours();
     var currentMin =  currentTime.getMinutes();
     var currentSec =  currentTime.getSeconds();
 
-    var localHour = 0;
+    var localHour;
+    var absLocalTimeZone;
 
-    // Logic to manually convert UTC to PST (UTC -7) ***CHANGE LOGIC IF USING OTHER TIMEZONES***
-    if(currentHour >= 0 && currentHour <= 6){
-        localHour = currentHour + 17;
+    // !!CHANGE THIS!! Enter difference in hours between your local time and UTC. 
+    var localTimeZone = 0;
+    /* Example: For PST (UTC -7), enter -7. For CET (UTC +1), enter 1. 
+    NOTE: Timezones with half and quarter hour offset such as IST are not supported. Enter nearest timezone with full hour offset, or enter 0 to keep it in UTC
+    NOTE 2.0: Do not try to enter invalid timezone values like numbers > 12, or letters. Your code will NOT work, you have been warned! */
+
+    // UTC to local timezone conversion
+    if (localTimeZone < 0){
+        absLocalTimeZone = localTimeZone * (-1);
+        if(currentHour >= 0 && currentHour < absLocalTimeZone){
+            localHour = currentHour + localTimeZone +24;
+        }
+        if(currentHour >= absLocalTimeZone && currentHour < 24){
+            localHour = currentHour + localTimeZone;
+        }
     }
 
-    if(currentHour >= 7 && currentHour <= 23){
-        localHour = currentHour - 7;
+    else if (localTimeZone > 0){
+        if(currentHour >= 0 && currentHour < (24 - localTimeZone)){
+            localHour = currentHour + localTimeZone;
+        }
+        if(currentHour >= (24 - localTimeZone) && currentHour < 24){
+            localHour = currentHour + localTimeZone - 24;
+        }
+    }
+
+    else{
+        localHour = currentHour;
     }
 
     // Convert minutes to 2 digits. eg: 5 to 05
@@ -60,16 +77,16 @@ exports.handler = (event, context, callback) => {
     var currentLocalTime = localHour + ':' + currentMin + ':' + currentSec;
     
     // Default is single click
-    var doorMessage = singleClick + currentLocalTime + localTimeZone;
+    var doorMessage = singleClick + currentLocalTime;
     
     // If button clicked twice
     if(event.clickType == "DOUBLE"){
-        doorMessage = doubleClick + currentLocalTime + localTimeZone;
+        doorMessage = doubleClick + currentLocalTime;
     }
 
     // If button long pressed
     if(event.clickType == "LONG"){
-        doorMessage = longClick + currentLocalTime + localTimeZone;
+        doorMessage = longClick + currentLocalTime;
     }
     
     const params = {
